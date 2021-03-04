6b397db0a14a78600b3fec29b1030e876355e95b
Mohamed <mohamedelyousfi87@gmail.com>
2021-03-04 14:29:52 +0100

    Update install.bat

--- install.bat
+++ install.bat
@@ -17,10 +17,8 @@ call :print "Unzipping PHP binaries"
 powershell.exe -nologo -noprofile -command "& { $shell = New-Object -COM Shell.Application; $target = $shell.NameSpace('%pocketminedir%'); $zip = $shell.NameSpace('%pocketminedir%\php.zip'); $target.CopyHere($zip.Items(), 16); }"
 
 call :print "Downloading composer"
-%php% -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
-%php% -r "if (hash_file('sha384', 'composer-setup.php') === '756890a4488ce9024fc62c56153228907f1545c228516cbf63f885e036d37e9a59d27d63f46af1d4d07ee0f76181c7d3') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
-%php% composer-setup.php
-%php% -r "unlink('composer-setup.php');"
+wget https://raw.githubusercontent.com/composer/getcomposer.org/76a7060ccb93902cd7576b67264ad91c8a2700e2/web/installer -O - -q | php -- --quiet
+
 
 call :print "Installing composer depedencies"
 %php% composer.phar install
